a259e3904eba1c130708a9dc51bee767bfcd3459
Harun Aşık <harun.esad@gmail.com>
2022-06-30 13:39:47 +0300

    commit

--- Assets/Scripts/JoystickControl.cs
+++ Assets/Scripts/JoystickControl.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class JoystickControl : MonoBehaviour
 {
@@ -14,8 +13,6 @@ public class JoystickControl : MonoBehaviour
     float turnSpeed = 5;
 
     public Animator playerAnim;
-
-    bool move;
     void Awake()
     {
         if (instance == null)
@@ -23,14 +20,9 @@ public class JoystickControl : MonoBehaviour
             instance = this;
         }
     }
-    private void Start()
-    {
-        move = true;
-    }
     void FixedUpdate()
     {
-        MoveLimit();
-        if (Input.GetButton("Fire1") && move)
+        if (Input.GetButton("Fire1"))
         {
             Joystick();
         }
@@ -44,26 +36,5 @@ public class JoystickControl : MonoBehaviour
 
         Vector3 direction = Vector3.forward * vertical + Vector3.right * horizontal;
         transform.rotation = Quaternion.Slerp(a: transform.rotation, b: Quaternion.LookRotation(direction), t: turnSpeed * Time.deltaTime);
-    }
-    void MoveLimit()
-    {
-        if (SceneManager.GetActiveScene().buildIndex == 0)
-        {
-            if (transform.position.z > 19)
-            {
-                move = false;
-            }
-            //float forward = Mathf.Clamp(transform.position.z, -21, 19);
-            //transform.position = new Vector3(transform.position.x, transform.position.y, forward);
-        }
-        else
-        {
-            if (transform.position.z > 61)
-            {
-                move = false;
-            }
-            //float forwardPos = Mathf.Clamp(transform.position.z, -21, 61);
-            //transform.position = new Vector3(transform.position.x, transform.position.y, forwardPos);
-        }
     }
 }
