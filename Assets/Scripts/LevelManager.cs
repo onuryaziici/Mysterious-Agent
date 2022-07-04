be355653a7086671c0fb1eb386c008b8464b9cbc
Onur <onuryaziici@gmail.com>
2022-07-04 12:57:54 +0300

    Joystick

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -99,7 +99,6 @@ public class LevelManager : MonoBehaviour
             level = score;
         }
         PlayerPrefs.SetFloat("Checkpoint", level);
-        Debug.Log(PlayerPrefs.GetFloat("Checkpoint"));
     }
     public void ScoreInc()
     {
